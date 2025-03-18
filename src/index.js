ecacc778336a4a342b87b2237f79e0f467d271ae
seoky99 <seokyoung99@gmail.com>
2025-03-18 21:21:48 +0900

    tab switching functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
 import { generateHomeContent } from "./home.js";
+import { generateMenuContent } from "./menu.js";
 import "./styles.css";
 
 generateHomeContent();
+
+function addListeners() {
+    const nav = document.querySelector("nav");
+    nav.addEventListener("click", (e) => {
+
+        if (e.target.matches("button")) {
+
+            const container = document.querySelector("#content");
+            container.replaceChildren();
+
+            switch (e.target.id) {
+                case "home":
+                    generateHomeContent(); 
+                    break;
+                case "menu":
+                    generateMenuContent();
+                    break;
+                default:
+                    break;
+                    
+            }
+            console.log(e.target.id);
+        }
+    });
+}
+
+addListeners();
--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,10 @@
 function generateMenuContent() {
-    let container = document.querySelector("#content");
     
+    let container = document.querySelector("#content");
+    const asd = document.createElement("h1");
+    asd.textContent = "Menu Page"
+    container.appendChild(asd);
+
 
 }
 
